a2fa66ecde93f1106be95b633a0205ba156cbe0d
kamusarihanshi <ahmedizaka9@gmail.com>
2024-06-09 09:46:54 +0300

    lab

--- lib/song.py
+++ lib/song.py
@@ -1,2 +1,35 @@
 class Song:
-    pass
+    count =0
+    genres=[]
+    artists=[]
+    genre_count={}
+   
+     
+    def __init__(self, name,artist,genre):
+        self.name=name
+        self.artist=artist
+        self.genre=genre
+    
+    @classmethod
+    def add_song_to_count(cls):
+        cls.count+=1
+    @classmethod
+    def add_to_genres(cls,genre):
+        if genre not in cls.genres:
+            cls.genres.append(genre)
+    @classmethod
+    def add_to_artists(cls,artist): 
+        cls.artists.append(artist)
+    @classmethod
+    def add_to_genre_count(cls,genre):
+     cls.genre_count[cls.genre]=cls.genre_count.get(genre,0)+1
+    @classmethod
+    def add_to_artist_count(cls,artist):
+     cls.artist_count[artist]=cls.artist_count.get(artist,0)+1
+    
+     def __repr__(self):
+        return f"{self.name} by {self.artist} in {self.genre}"
+     
+
+     
+       
